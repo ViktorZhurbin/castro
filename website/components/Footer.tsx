bd51d347db268db2a80a31c212759d8292554760
Viktor Zhurbin <34838167+ViktorZhurbin@users.noreply.github.com>
2026-04-12 17:52:26 +0200

    theme tweaks

--- website/components/Footer.tsx
+++ website/components/Footer.tsx
@@ -3,28 +3,30 @@ import "./Footer.css";
 export function Footer() {
 	return (
 		<footer class="footer">
-			{/* Slogan locked hard left */}
-			<div class="footer-slogan">
-				<p>WORKERS OF THE WEB, UNITE!</p>
-				<p>SEIZE THE MEANS OF RENDERING.</p>
-			</div>
+			<div>
+				{/* Slogan locked hard left */}
+				<div class="footer-slogan">
+					<p>WORKERS OF THE WEB, UNITE!</p>
+					<p>SEIZE THE MEANS OF RENDERING.</p>
+				</div>
 
-			{/* Utilities anchored right */}
-			<div class="footer-nav-section">
-				<nav class="footer-nav">
-					<a href="/about">About</a>
-					<a
-						href="https://github.com/ViktorZhurbin/castro"
-						target="_blank"
-						rel="noopener"
-					>
-						GitHub
-					</a>
-				</nav>
-				<p class="footer-info">
-					Built with Castro <br />
-					The People's Framework
-				</p>
+				{/* Utilities anchored right */}
+				<div class="footer-nav-section">
+					<nav class="footer-nav">
+						<a href="/about">About</a>
+						<a
+							href="https://github.com/ViktorZhurbin/castro"
+							target="_blank"
+							rel="noopener"
+						>
+							GitHub
+						</a>
+					</nav>
+					<p class="footer-info">
+						Built with Castro <br />
+						The People's Framework
+					</p>
+				</div>
 			</div>
 		</footer>
 	);
