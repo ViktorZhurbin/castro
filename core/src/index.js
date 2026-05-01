6ec845b51826bf00e956e09cd59f73a20bff1a12
Viktor Zhurbin <34838167+ViktorZhurbin@users.noreply.github.com>
2026-05-01 16:49:06 +0200

    add comment

--- core/src/index.js
+++ core/src/index.js
@@ -1,2 +1,6 @@
-/** @type {import('./types').defineConfig} */
+/**
+ * Identity function that provides type inference for castro config file
+ *
+ * @type {import('./types').defineConfig}
+ */
 export const defineConfig = (config) => config;
