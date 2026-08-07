acaef266aeaecf398569479218498f3f8543334d
Viktor Zhurbin <34838167+ViktorZhurbin@users.noreply.github.com>
2026-08-07 23:41:39 +0200

    add cx util

--- website/src/pages/_components/index/CTAButtons.tsx
+++ website/src/pages/_components/index/CTAButtons.tsx
@@ -1,8 +1,12 @@
 import "./CTAButtons.css";
 
+function cx(...classes: Array<string | null | undefined>) {
+  return classes.filter(Boolean).join(" ");
+}
+
 export function CTAButtons(props: { className?: string }) {
   return (
-    <div class={`cta-buttons-container ${props.className}`}>
+    <div class={cx("cta-buttons-container", props.className)}>
       <a
         href="https://github.com/ViktorZhurbin/castro"
         target="_blank"
