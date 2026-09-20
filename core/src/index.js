2e7e857cacac02695c3190c1ca201b84dc942a39
Viktor Zhurbin <34838167+ViktorZhurbin@users.noreply.github.com>
2026-09-20 14:27:39 +0200

    typescript v7

--- core/src/index.js
+++ core/src/index.js
@@ -1,6 +1,6 @@
 /**
  * Identity function that provides type inference for castro config file
  *
- * @type {import('./types').defineConfig}
+ * @type {typeof import('./types').defineConfig}
  */
 export const defineConfig = (config) => config;
